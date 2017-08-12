e9e59b7f7b39f2d6c42ab16adf527e43adc3b49a
bobocoder <bobocoder@users.noreply.github.com>
2017-08-11 22:09:53 -0400

    kthmax experiments

--- data-structures/src/bobo/Program.java
+++ data-structures/src/bobo/Program.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import bobo.algorithms.HeapSort;
 import bobo.structures.Heap;
+import bobo.structures.Heap.SortOrder;
 import bobo.structures.HeapAsArray;
 
 public class Program {
@@ -13,6 +14,48 @@ public class Program {
   };
 
   public static void main( String[] args ) {
+    kthmax();
+  }
+
+  private static void kthmax() {
+    int count = 100;
+    int k = 5;
+
+    int[] rawData = generateData( count, Distribution.RAND );
+    int[][] data =
+        { rawData.clone(), rawData.clone(), rawData.clone(), rawData.clone(), rawData.clone() };
+    int[][] results = new int[5][k]; // 5 solutions
+
+    // solution 1
+    int sln1Count = count;
+    for ( int i = 0; i < k; i++ ) {
+      int maxIndex = 0;
+      for ( int j = 0; j < sln1Count; j++ ) {
+        if ( data[0][j] > data[0][maxIndex] ) {
+          maxIndex = j;
+        }
+      }
+      sln1Count--;
+      results[0][i] = data[0][maxIndex];
+      data[0][maxIndex] = data[0][sln1Count];
+    }
+
+    // solution 2
+    HeapSort.heapSort2( data[1], SortOrder.MAX );
+    for ( int i = 0; i < k; i++ ) {
+      results[1][i] = data[1][count - i - 1];
+    }
+
+    for ( int h = 0; h < k; h++ ) {
+      System.out.println( "Solution " + ( h + 1 ) );
+      for ( int i : results[h] ) {
+        System.out.print( i + ", " );
+      }
+      System.out.println();
+    }
+  }
+
+  private static void heapExperiments( String[] args ) {
     int count = 50;
     int[] data = generateData( count, Distribution.RAND );
 
